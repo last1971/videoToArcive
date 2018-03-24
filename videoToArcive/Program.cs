4fab6af840bab56eaf6bb9c1888c7060c903c4cf
Владимир Ластовка <last@rostown.ru>
2018-03-24 18:07:17 +0300

    last

--- videoToArcive/Program.cs
+++ videoToArcive/Program.cs
@@ -49,60 +49,18 @@ namespace videoToArcive
         static extern bool PostMessage(IntPtr hWnd, uint Msg, int wParam, int lParam);
         [DllImport("User32.dll")]
         static extern int SendMessage(IntPtr hWnd, int uMsg, int wParam, string lParam);
-        [DllImport("user32.dll")]
-        static extern int SendMessageA(IntPtr hWnd, uint Msg, int wParam, IntPtr lParam);
-        [DllImport( "user32.dll", SetLastError = true)]
-        static extern int GetWindowThreadProcessId(IntPtr hWnd, out int lpdwProcessId);
-        [DllImport("kernel32.dll", SetLastError = true)]
-        static extern IntPtr OpenProcess(uint dwDesiredAccess, bool bInheritHandle, int dwProcessId);
-        [DllImport("kernel32.dll")]
-        static extern IntPtr VirtualAllocEx(IntPtr hProcess, IntPtr lpAddress,uint dwSize, uint flAllocationType, uint flProtect);
-        [DllImport("kernel32.dll", SetLastError = true)]
-        static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, int dwSize, ref int lpNumberOfBytesWritten);
-        [DllImport("kernel32.dll")]
-        public static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, int dwSize, ref int lpNumberOfBytesRead);
         const int WM_SETTEXT = 0x000c;
         const uint WM_KEYDOWN = 0x0100;
-        const int LVM_GETITEMCOUNT = 0x1004;
-        const uint LVIF_TEXT = 0x00000001;
-        const int LVM_GETITEM = 0x1005;
-        const int PROCESS_VM_WRITE = 0x0020;
-        const int PROCESS_VM_OPERATION = 0x0008;
-        const int MEM_COMMIT = 0x00001000;
-        const int PAGE_READWRITE = 0x04;
-        [StructLayoutAttribute(LayoutKind.Sequential)]
-        private struct LVITEM
-        {
-            public uint mask;
-            public int iItem;
-            public int iSubItem;
-            public uint state;
-            public uint stateMask;
-            public IntPtr pszText;
-            public int cchTextMax;
-            public int iImage;
-            public IntPtr lParam;
-        }
+        
         static void Main(string[] args)
         {
             string sevenZ = @"c:\7ZIP\7zFM.exe";
             if (File.Exists(sevenZ))
             {
-                Process.Start(sevenZ);//Запускаем архиватор
+                Process p = Process.Start(sevenZ);//Запускаем архиватор
                 Thread.Sleep(1000);
-                IntPtr h1 = (IntPtr)0;
-                string caption = "";
-                int id = 0;
-                //List<int> dir = new List<int> { 0x2E, 0x43, };
-                foreach (System.Diagnostics.Process anti in System.Diagnostics.Process.GetProcesses()) // перебираем все процесы
-                {
-                    if (anti.ProcessName.Contains("7zFM")) // находим окно по точному заголовку окна
-                    {
-                        h1 = anti.MainWindowHandle;
-                        caption = anti.MainWindowTitle;
-                        id = anti.Id;
-                    }
-                }
+                IntPtr h1 = p.MainWindowHandle;
+                string caption = p.MainWindowTitle;
                 if (h1!=null)
                 {
                     IntPtr h2 = FindWindowEx(h1,new IntPtr(0), "7-Zip::Panel", "");
@@ -157,8 +115,7 @@ namespace videoToArcive
                         PostMessage(h3, WM_KEYDOWN, 13, 0);
                         Console.WriteLine("Архив создан");
                         Thread.Sleep(2000);
-                        Process Id = Process.GetProcessById(id);
-                        Id.Kill();
+                        p.Kill();
                     }
                     else
                     {
